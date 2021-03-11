bc694245e5ff0da83102175c5264b5d38fb2a75a
VeranikaKaraliova <nika-11@mail.ru>
2021-03-11 20:49:02 +0300

    feat: add min, max, and svg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
+exports.min = function min (array=[]) {
+  if(array.length > 0){
+    let minNum = array[0];
+    for(let i=0; i<array.length;i++){
+      if(minNum > array[i]){
+        minNum = array[i];
+      };
+    };
+    return minNum;
+  };
   return 0;
 }
 
-exports.max = function max (array) {
+exports.max = function max (array=[]) {
+  if(array.length > 0){
+    let maxNum = array[0];
+    for(let i=0; i<array.length;i++){
+      if(maxNum < array[i]){
+        maxNum = array[i];
+      };
+    };
+    return maxNum;
+  };
   return 0;
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg (array=[]) {
+  if(array.length >0){
+    let sumArr = array.reduce(function(sum, current) {
+      return sum + current;
+    }, 0);
+    return sumArr / array.length;
+  };
   return 0;
 }
